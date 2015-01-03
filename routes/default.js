2bddd2cf5cddcef903dad11e73bb687fe533d612
Jérôme LELEU <leleuj@gmail.com>
2015-01-03 10:51:30 +0100

    handle SLO, update dependencies and manage user attributes

--- routes/default.js
+++ routes/default.js
@@ -10,18 +10,30 @@ getUser = function(req) {
   return user;
 }
 
+getAttributes = function(req) {
+  var attributes = '';
+  if (req.session.cas && req.session.cas.attributes) {
+    attributes = JSON.stringify(req.session.cas.attributes);
+  }
+  return attributes;
+}
+
 f = function(req, res) {
-  res.render('index', {user: getUser(req)});
+  res.render('index', {user: getUser(req), attributes: getAttributes(req)});
 }
 
 router.get('/', f);
 
 router.get('/index', f);
 
-router.get('/protected/index', cas.serviceValidate(), cas.authenticate(), function(req, res) {
-  res.render('protected', {user: getUser(req)});
+// cas.ssout('http://localhost:3000') : handle logout requests directly from the CAS server
+// cas.serviceValidate()              : validate service tickets received from the CAS server
+// cas.authenticate()                 : request an authentication if the user is not authenticated
+router.get('/protected/index', cas.ssout('http://localhost:3000'), cas.serviceValidate(), cas.authenticate(), function(req, res) {
+  res.render('protected', {user: getUser(req), attributes: getAttributes(req)});
 });
 
+// handle application logouts from the CAS logout page (in the browser)
 router.get('/logout', function(req, res) {
   if (req.session.destroy) {
     req.session.destroy();
